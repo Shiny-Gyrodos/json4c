48ff2c3b9e1f21a71e7a6720e6395431f08e6ea4
Shiny Gyrodos <gibbs2kids@gmail.com>
2025-08-23 18:13:54 -0500

    added 'IS_X' macros for testing the type of a JsonNode*, add macros for creating empty json_objects and json_arrays

--- json4c.h
+++ json4c.h
@@ -48,6 +48,12 @@ typedef struct JsonValue {
 #define AS_STRING(jnode)	((jnode)->value.string)
 #define AS_ARRAY(jnode)		((jnode)->value.jcomplex)
 #define AS_OBJECT(jnode)	((jnode)->value.jcomplex)
+#define IS_INT(jnode)		((jnode)->value.type == JSON_INT)
+#define IS_REAL(jnode)		((jnode)->value.type == JSON_REAL)
+#define IS_BOOL(jnode)		((jnode)->value.type == JSON_BOOL)
+#define IS_STRING(jnode)	((jnode)->value.type == JSON_STRING)
+#define IS_ARRAY(jnode)		((jnode)->value.type == JSON_ARRAY)
+#define IS_OBJECT(jnode)	((jnode)->value.type == JSON_OBJECT)
 typedef struct JsonNode {
 	char* identifier;
 	struct JsonValue value;
@@ -61,8 +67,10 @@ void jnode_free(JsonNode*);
 // Serialization
 JsonNode* _json_object(JsonNode**);
 #define json_object(...) _json_object((JsonNode*[]){__VA_ARGS__, NULL})
+#define json_emptyObject() jnode_create(NULL, (JsonValue){JSON_OBJECT, 0})
 JsonNode* _json_array(JsonNode**);
 #define json_array(...) _json_array((JsonNode*[]){__VA_ARGS__, NULL})
+#define json_emptyArray() jnode_create(NULL, (JsonValue){JSON_ARRAY, 0})
 JsonNode* json_bool(bool);
 JsonNode* json_int(int);
 JsonNode* json_real(float);
